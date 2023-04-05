fd9d79145d0650962fb5a0c51fa2226b650d0c70
Devesh Sawale <im007dev@gmail.com>
2023-04-05 22:27:09 +0530

    post model created

--- server/index.js
+++ server/index.js
@@ -2,12 +2,18 @@ import express from "express";
 import cors from "cors";
 import * as dotenv from "dotenv";
 import connectDB from "./mongodb/connect.js";
+import postRoutes from './routes/postRoutes.js'
+import imgRoutes from './routes/imgRoutes.js'
 
 dotenv.config()
 
 const app = express()
 app.use(cors())
 app.use(express.json({ limit : '45mb'}))
+
+app.use('api/post', postRoutes)
+app.use('api/img', imgRoutes)
+
 
 app.get('/', async (req,res) => {
     res.send("Hello World!")

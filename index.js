31638b6f998699a703dd508d1da7a3a6c8b7cfdb
Devesh Sawale <im007dev@gmail.com>
2023-04-08 11:22:02 +0530

    backend code

---
+++ index.js
@@ -0,0 +1,31 @@
+import express from "express";
+import cors from "cors";
+import * as dotenv from "dotenv";
+import connectDB from "./mongodb/connect.js";
+import postRoutes from './routes/postRoutes.js'
+import imgRoutes from './routes/imgRoutes.js'
+
+dotenv.config()
+
+const app = express()
+app.use(cors())
+app.use(express.json({ limit : '45mb'}))
+
+app.use('/api/post', postRoutes)
+app.use('/api/img', imgRoutes)
+
+
+app.get('/', async (req,res) => {
+    res.send("Hello World!")
+})
+
+const runserver = async () => {
+    try {
+        connectDB(process.env.MONGO_URL)
+        app.listen(8080, () => console.log("Server started on port http://localhost:8080"))
+    } catch (error) {
+        console.log(error);   
+    }
+}
+
+runserver()
